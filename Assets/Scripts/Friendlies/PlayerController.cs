24d884b5b903b205f74590fce75f58ee302181fa
Kelsey <32395780+KelseyMichelle@users.noreply.github.com>
2022-11-27 17:33:06 -0800

    Middlin

--- Assets/Scripts/Friendlies/PlayerController.cs
+++ Assets/Scripts/Friendlies/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,11 +16,16 @@ public class PlayerController : MonoBehaviour
     public bool canAttack;
     public float speed;
     public float maxSpeed;
+    public Rigidbody2D balloon;
+    public Text distanceText;
+    public float minStretchDistance;
+    public float maxStretchDistance;
 
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
     private Vector2 movementInput = Vector2.zero;
     private Vector2 lookInput = Vector2.zero;
+    
 
     private void Start()
     {
@@ -62,11 +67,14 @@ public class PlayerController : MonoBehaviour
         Vector2 curMovement = movementInput * playerSpeed * Time.deltaTime * speed;
         Vector2 curRotation = new Vector2(lookInput.y, -lookInput.x);
         Quaternion playerRotation = Quaternion.LookRotation(curRotation, Vector3.forward);
+        distanceText.text = Vector2.Distance(transform.position, balloon.position).ToString();
+
+
         rb.SetRotation(playerRotation);
 
         rb.velocity = rb.velocity + curMovement;
 
-        // Changes the height position of the player..
+        // Changes the height position of the   player..
         if (attacked && canAttack)
         {
             Attack();
